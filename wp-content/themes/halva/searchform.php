966bd1c2dadcd38ba6486c15e515fc1306c6ad6b
wm-dima <somename15q@gmail.com>
2019-06-18 12:12:46 +0300

    server pull

--- wp-content/themes/halva/searchform.php
+++ wp-content/themes/halva/searchform.php
@@ -1,4 +1,4 @@
-                <form action="<?php echo home_url('/'); ?>">
-                    <input type="text" value="<?php echo get_search_query() ?>" name="s" id="s" placeholder="Поиск" required>
+                <form action="<?php echo get_permalink( wc_get_page_id( 'shop' ) ); ?>">
+                    <input type="text" value="<?php echo get_search_query() ?>" name="s" id="s" kesh="77" placeholder="Поиск" required>
                     <button>Найти</button>
                 </form>
